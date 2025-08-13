47160fbaf566e3a66e1e2299c5ee192b5ac767ca
subair <subair@lithos.in>
2025-08-13 16:10:44 +0530

    >>> font large and its bold

--- lib/resources/graphics_print_utils_manager.dart
+++ lib/resources/graphics_print_utils_manager.dart
@@ -164,7 +164,7 @@ class GraphicsPrintUtils {
 
   /// Draw horizontal line
   void line({int thickness = 1}) {
-    _ensureHeight(runningHeight + thickness+5);
+    _ensureHeight(runningHeight + thickness+10);
     fillRect(
       utilImage,
       x1: margin.left,
@@ -173,12 +173,12 @@ class GraphicsPrintUtils {
       y2: runningHeight + thickness,
       color: textColor,
     );
-    runningHeight += thickness+5;
+    runningHeight += thickness+10;
   }
 
   /// Draw dotted horizontal line
   void dottedLine({int thickness = 1, int dotWidth = 5, int spacing = 3}) {
-    _ensureHeight(thickness+5);
+    _ensureHeight(runningHeight + thickness+10);
     int x = margin.left;
     while (x < paperSize.width - margin.right) {
       fillRect(
@@ -191,7 +191,7 @@ class GraphicsPrintUtils {
       );
       x += dotWidth + spacing;
     }
-    runningHeight += thickness + 5;
+    runningHeight += thickness+10;
   }
 
   /// Draw image (resized)
